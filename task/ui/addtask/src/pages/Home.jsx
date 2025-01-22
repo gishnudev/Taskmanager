3771ce41115c137d767035fb7c7f4690f71fe823
Gishnu Devadathan <jishnudevadathan1998@gmail.com>
2025-01-22 15:51:56 +0530

    FINAl

---
+++ task/ui/addtask/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <>
+    <div className='text-4xl text-black font-bold text-center p-8'>Task manager</div>
+    <button className='bg' >Add Task</button>
+    </>
+    
+  )
+}
+
+export default home
